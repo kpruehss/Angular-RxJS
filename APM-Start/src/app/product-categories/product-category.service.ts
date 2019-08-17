1fdf1527b49ffc0dfd5df2f001dbbe16d64639cb
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2019-08-17 14:52:02 +1000

    combining category product streams
    - All streams successfully combined.
    - New observable with category field in product via lookup
    - Tested and working

--- APM-Start/src/app/product-categories/product-category.service.ts
+++ APM-Start/src/app/product-categories/product-category.service.ts
@@ -4,14 +4,22 @@ import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 
 import { ProductCategory } from './product-category';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductCategoryService {
   private productCategoriesUrl = 'api/productCategories';
 
-  constructor(private http: HttpClient) { }
+  productCategories$ = this.http
+    .get<ProductCategory[]>(this.productCategoriesUrl)
+    .pipe(
+      tap(data => console.log('categories', JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+
+  constructor(private http: HttpClient) {}
 
   private handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
