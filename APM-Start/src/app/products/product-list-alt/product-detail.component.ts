bb7b328280e3bf74884f1332175a9c4cef7f1fa3
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2019-08-23 09:34:56 +1000

    Ancillary Streams

--- APM-Start/src/app/products/product-list-alt/product-detail.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-detail.component.ts
@@ -1,8 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 import { ProductService } from '../product.service';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { EMPTY } from 'rxjs';
+import { Product } from '../product';
 
 @Component({
   selector: 'pm-product-detail',
@@ -10,7 +11,6 @@ import { EMPTY } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProductDetailComponent {
-  pageTitle = 'Product Detail';
   errorMessage = '';
 
   product$ = this.productService.selectedProduct$.pipe(
@@ -20,12 +20,16 @@ export class ProductDetailComponent {
     })
   );
 
+  pageTitle$ = this.product$.pipe(
+    map((p: Product) => (p ? `Product Detail for: ${p.productName}` : null))
+  );
+
   productSuppliers$ = this.productService.selectedProductSuppliers$.pipe(
     catchError(err => {
       this.errorMessage = err;
       return EMPTY;
     })
-  )
+  );
 
   constructor(private productService: ProductService) {}
 }
